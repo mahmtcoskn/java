339e0e8338f78cc0562919b5b25883088e122680
mahmtcoskn <mhmt_coskun@icloud.com>
2023-01-06 22:53:18 +0300

    first commit

---
+++ src/day27_overriding/GExceptions.java
@@ -0,0 +1,20 @@
+package day27_overriding;
+
+public class GExceptions {
+    public static void main(String[] args) {
+
+        int a = 23;
+        int b = 0;
+
+
+        if (b != 0){
+            System.out.println("Bölüm = " + a/b);
+        }else {
+            System.out.println("Bölecek sayı sifir olamaz !!");
+        }
+
+
+
+
+    }
+}
---
+++ src/day27_overriding/HExceptions.java
@@ -0,0 +1,17 @@
+package day27_overriding;
+
+public class HExceptions {
+    public static void main(String[] args) {
+
+
+        String str = "Java";
+        Integer[] arr = {1, 2, 3, 4};
+
+        int strIndex = 3;
+        int arrIndex = 2;
+
+        System.out.println("" + str.charAt(strIndex) + arr[arrIndex]);
+
+
+    }
+}
