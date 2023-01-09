945c248e190776da6530977b9971e185cb891a3f
mahmtcoskn <mhmt_coskun@icloud.com>
2023-01-09 03:46:03 +0300

    first commit

---
+++ src/day30_interfaces_iterator/I01_Interface.java
@@ -0,0 +1,42 @@
+package day30_interfaces_iterator;
+
+public interface I01_Interface {
+
+    // Interface lerde tum method lar public ve abstract tir.
+    // Interface lerde tum method lar public, static ve final dır.
+    // final oldugundan sonradan deger atamak mümkün olmaz, olustururken deger atanmalidir.
+
+    int SAYI=10;
+    public static final String OKUL_ISMI="Yıldız Koleji";
+    public int SAYI2=20;
+
+    void method1();
+    public int method2();
+    abstract String method3();
+    public abstract void method4();
+    void method5();
+
+    /*
+        Bir interface e sonradan bir method eklemek istedigimizde
+        o interface i daha once implement eden tum class lar
+        yeni eklenen method u implement etmek zorunda olur.
+
+        Java ya yapilan talepler sonucunda
+        Java 8 ile birlikte bu konuda bir istisna getirilmiştir
+        Eger sonradan eklenen method un tum eski class lar tarafindan implement edilme mecburiyeti
+        olmasin istiyorsaniz
+
+        yeni olusturdugunuz method a DEFAULT veya STATIC keyword ekleyip
+        body de {} olusturursaniz
+        o zaman bu method un child class lar tarafindan
+        implement edilme MECBURIYETI OLMAZ
+
+     */
+
+    public default void method44(){
+        System.out.println("Interface deki default olarak isaretlenen method calisti");
+    }
+    public static void method34(){
+        System.out.println("Interface deki static olarak isaretlenen method calisti");
+    }
+}
