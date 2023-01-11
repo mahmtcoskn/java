370691c7f746cd3b1e3370d5ffb1acb508dfd497
mahmtcoskn <mhmt_coskun@icloud.com>
2023-01-11 19:34:24 +0300

    first commit

--- src/day31_collections/C04_Deque.java
+++ src/day31_collections/C04_Deque.java
@@ -8,21 +8,28 @@ public class C04_Deque {
         // Deque double ended queue demektir
         // iki yonlu kuyruk
         // Bu iki yonlu yapisindan dolayi pek cok method'un first() ve last() versiyonu vardir
+
         Deque<String> harfler = new LinkedList<>();
         harfler.add("J");
         harfler.add("L");
+
         System.out.println(harfler); // [J, L]
         harfler.addFirst("B");
         System.out.println(harfler); // [B, J, L]
+
         System.out.println(harfler.removeFirst()); // B
         System.out.println(harfler); // [J, L]
+
         harfler.addFirst("A");
         harfler.addLast("A");
         harfler.add("K");
+
         System.out.println(harfler); //  [A, J, L, A, K]
+
         // sona dogru olan A'yi silin
         System.out.println(harfler.removeLastOccurrence("A")); // true
         System.out.println(harfler); //  [A, J, L, K]
+
         System.out.println(harfler.removeLastOccurrence("T")); // false
         LinkedList<String> karakterler = new LinkedList<>();
         karakterler.push("A"); // addFirst() ile ayni islemi yapar
--- src/day31_collections/C06_ArraydekiTekrarlardanKurtulma.java
+++ src/day31_collections/C06_ArraydekiTekrarlardanKurtulma.java
@@ -5,19 +5,26 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class C06_ArraydekiTekrarlardanKurtulma {
+
     public static void main(String[] args) {
         // verilen bir arraydeki tekrar eden elementleri
         // sadece 1 kere yazarak array'i kisaltan kod yazin
+
         int[] arr = {1, 2, 4, 2, 3, 5, 6, 2, 4, 5, 1, 2, 4, 5, 3, 2, 5, 6, 1, 2, 5, 4, 3};
+
         Set<Integer> arrSet = new HashSet<>();
+
         // array'deki tum elementleri, Set'e ekleyelim
+
         for (int each : arr
         ) {
             arrSet.add(each);
         }
         System.out.println(arrSet); // [1, 2, 3, 4, 5, 6]
+
         // Set'deki element sayisina esit bir array olusturup
         // Set'deki elementleri oraya tasiyalim
+
         arr = new int[arrSet.size()]; // [0, 0, 0, 0, 0, 0]
         int index = 0;
         for (int each : arrSet
