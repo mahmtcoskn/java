53ee5f4544be9d7a431b7e87cedbd586ea000bbf
mahmtcoskn <mhmt_coskun@icloud.com>
2023-01-18 02:17:01 +0300

    first commit

---
+++ src/day36_genelTekrar/C02_StringManipulation.java
@@ -0,0 +1,14 @@
+package day36_genelTekrar;
+
+public class C02_StringManipulation {
+
+    public static void main(String[] args) {
+
+        // Verilen bir cumlede istenen kelimenin kac kere gectigini bulup yazdiran
+        // bir method olusturalim
+
+        String cumle = "Java basladi ve bitti. Java meger ne guzelmis. Ne phyton ne C, Java en iyisi";
+
+        String istenenKelime= "Java";
+    }
+}
