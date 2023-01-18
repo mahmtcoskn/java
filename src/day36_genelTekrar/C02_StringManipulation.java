37dacad4863bf115b6e58c7f6eabf5e40db0c369
mahmtcoskn <mhmt_coskun@icloud.com>
2023-01-18 20:47:50 +0300

    first commit

--- src/day36_genelTekrar/C02_StringManipulation.java
+++ src/day36_genelTekrar/C02_StringManipulation.java
@@ -10,5 +10,64 @@ public class C02_StringManipulation {
         String cumle = "Java basladi ve bitti. Java meger ne guzelmis. Ne phyton ne C, Java en iyisi";
 
         String istenenKelime= "Java";
+
+        //kullanimSayisiBul1(cumle,istenenKelime);
+        //kullanimSayisiBul2(cumle,istenenKelime);
+        kullanimSayisiBul3(cumle,istenenKelime);
+    }
+
+    private static void kullanimSayisiBul3(String cumle, String istenenKelime) {
+
+        // Java buldukca silerek yapildi
+        // ama JaJavava varsa 5 adet buluyor
+        // bunun yerine _ kullandik ve sorun cozuldu.
+
+        int kullanimSayisi = 0;
+        while (cumle.contains(istenenKelime)){
+            kullanimSayisi ++;
+            cumle = cumle.replaceFirst(istenenKelime,"_");
+        }
+        if (kullanimSayisi == 0) {
+            System.out.println("Aranan kelime cumlede yok");
+        } else {
+            System.out.println("Kelime cumlede " + kullanimSayisi + " adet kullanilmistir");
+        }
+    }
+
+    private static void kullanimSayisiBul2(String cumle, String istenenKelime) {
+        int kullanimSayisi = 0;
+
+        // forloop ve substring ile yapalim
+
+        for (int i = 0; i <= cumle.length() - istenenKelime.length(); i++) {
+            if (cumle.substring(i, i + istenenKelime.length()).equals(istenenKelime)) {
+                kullanimSayisi++;
+            }
+        }
+        if (kullanimSayisi == 0) {
+            System.out.println("Aranan kelime cumlede yok");
+        } else {
+            System.out.println("Kelime cumlede " + kullanimSayisi + " adet kullanilmistir");
+        }
+    }
+    private static void kullanimSayisiBul1(String cumle, String istenenKelime) {
+        int kullanimSayisi = 0;
+        int index = 0;
+
+        while (index<cumle.length()){
+            index = cumle.indexOf(istenenKelime,index);
+            if (index == -1){
+                if (kullanimSayisi == 0){
+                    System.out.println("Aranan kelime cumlede yok");
+                    break;
+                }else {
+                    System.out.println("Kelime cumlede " + kullanimSayisi + " adet kullanilmistir");
+                    break;
+                }
+            }else {
+                kullanimSayisi++;
+                index++;
+            }
+        }
     }
 }
